12117bb32a9a2588e8da3ef3493af21ad5268967
Karen V <karenvo@usc.edu>
2020-01-18 09:26:05 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -13,18 +13,18 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" 
-            name="username" 
-            type="text" 
+            <input id="username"
+            name="username"
+            type="text"
             />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" 
-            name="password" 
-            type="password" 
+            <input id="password"
+            name="password"
+            type="password"
             />
           </label>
         </div>
